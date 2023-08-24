c6444780e757fef5cb1ab166b59fdd58db5a9497
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-08-24 18:29:29 +0800

    add fuzzrepair instrumentation

--- libtiff/bugzilla-2633/fuzzrepair/instrument.sh
+++ libtiff/bugzilla-2633/fuzzrepair/instrument.sh
@@ -12,23 +12,8 @@ cd $dir_name/src
 sed -i '2695s/^.*$/while (cc-- > 1) {/' tools/tiff2ps.c
 
 # (2) add instrumentation for fuzzrepair
-$script_dir/../config.sh $1
 cd $dir_name/src
 sed -i '2470i if(0) return;' tools/tiff2ps.c
 sed -i '2441i if(0) return;' tools/tiff2ps.c
-make clean
 
-bear $script_dir/../build.sh $1
-cd $LIBPATCH_DIR/rewriter
-./rewritecond $fix_file -o $fix_file
-ret=$?
-if [[ ret -eq 1 ]]; then
-   exit 128
-fi
 
-# build with AFL instrumentation
-cd $dir_name/src/
-make clean
-make distclean
-CC="afl-clang-fast" CXX="afl-clang-fast++" R_CFLAGS="-g -O0 -fsanitize=address" R_CPPFLAGS="-g -O0 -fsanitize=address" R_LDFLAGS="-pie" $script_dir/../config.sh $1
-R_CFLAGS="-fsanitize=address -g -O0 -fPIE" R_CPPFLAGS="-fsanitize=address -g -O0 -fPIE" R_LDFLAGS="-pie -fsanitize=address" $script_dir/../build.sh $1
